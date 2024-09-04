a4208f74d914418238a80b20ba3154be1da70c90
selan0928 <122727676+selan0928@users.noreply.github.com>
2024-09-04 14:03:14 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -894,7 +894,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rustdesk-api.boomead.com:666".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
